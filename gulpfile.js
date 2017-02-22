7cb6b99089ce8b702f81ee2152423ffa9f71f0a1
padamsson <patricia.adamsson@gmail.com>
2017-02-22 22:50:49 +0100

    Adds sBuild Sass

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var pathSass = './ui/style/sass/';
+// var minifyCSS = require('gulp-minify-css');
+
+gulp.task('sass', function () {
+  return gulp.src(pathSass + 'all.scss')
+    .pipe(concat('style.css'))
+    .pipe(sass().on('error', sass.logError))
+    // .pipe(minifyCSS())
+    .pipe(gulp.dest('./dist/style'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(pathSass + '**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
